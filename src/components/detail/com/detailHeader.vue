d733b3ef992198d34ba5bac25ac780b9ee4fa09d
tom <294274944@qq.com>
2019-05-24 21:37:17 +0800

    头部样式完成

---
+++ src/components/detail/com/detailHeader.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="header">
+            <div class="header-back"><span class="iconfont icon-back">&#xe64c;</span></div>
+            <div class="header-like"><span class="iconfont icon-like">&#xe60c;</span>220</div>
+            <div class="header-comment"><span class="iconfont icon-comment">&#xe638;</span>38</div>
+        <div class="header-star"><span class="iconfont icon-star">&#xe61b;</span></div>
+        <div class="header-share"><span class="iconfont icon-share">&#xe624;</span></div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'detailHeader',
+        components: {
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .header {
+        width: 100%;
+        background-color: @baseBlueColor;
+        overflow: hidden;
+        font-size: .35rem;
+        color: @fWhite;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 5;
+        padding-top: 5%;
+
+    &.header-night {
+         background-color: @baseBlueColorNight;
+     }
+
+    div {
+        display: inline-block;
+        /*height: 1.1rem;*/
+        /*line-height: 1.1rem;*/
+        padding-bottom: 10%;
+        height: 0;
+        margin: 0 .1rem;
+        /*box-sizing: border-box;*/
+        &.header-star, .header-share {
+            margin: 0 .35rem;
+        }
+    }
+
+    .header-back{
+        float: left;
+        margin: 0 0 0 .25rem;
+    }
+
+    .header-comment, .header-like, .header-star, .header-share{
+        float: right;
+    }
+    .icon-comment, .icon-like {
+        margin-right: .1rem;
+    }
+
+    /*.icon-comment {*/
+    /*    font-size: .5rem;*/
+    /*}*/
+
+    }
+</style>
--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -1,13 +1,30 @@
 <template>
     <div class="detail">
-    1111
+        <detail-header></detail-header>
     </div>
 </template>
 
 <script>
-
+    import axios from 'axios'
+    import detailHeader from 'detail/com/detailHeader'
     export default {
-        name: 'detail'
+        name: 'detail',
+        components: {
+            detailHeader
+        },
+        methods: {
+            getDetailData() {
+                axios.get('/api/4/news/9711670')
+                    .then(this.getDetailDataSucc)
+            },
+            getDetailDataSucc(ret) {
+                const data = ret.data
+                console.log(data)
+            }
+        },
+        mounted() {
+            this.getDetailData()
+        }
     }
 </script>
 
