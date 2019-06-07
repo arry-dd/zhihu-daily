3adfa23fc836e7c97aa200e77d9840a4a2a31e35
tom <294274944@qq.com>
2019-06-07 21:40:32 +0800

    修改ico

--- vue.config.js
+++ vue.config.js
@@ -33,5 +33,14 @@ module.exports = {
         preProcessor: 'less',
         patterns: [resolve('src/assets/styles/variable.less')]
       }
-    }
+    },
+    pwa: {
+        iconPaths: {
+          favicon32: 'favicon.ico',
+          favicon16: 'favicon.ico',
+          appleTouchIcon: 'favicon.ico',
+          maskIcon: 'favicon.ico',
+          msTileImage: 'favicon.ico'
+        }
+      }
 }
